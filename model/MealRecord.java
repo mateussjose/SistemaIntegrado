528a2113bee74851c9f7b61a46d1522f91304cab
Mateus Oliveira <76123045+mateussjose@users.noreply.github.com>
2025-11-11 12:38:29 -0300

    Change return type of getPaciente method to Pessoa

--- model/MealRecord.java
+++ model/MealRecord.java
@@ -6,5 +6,5 @@ public class MealRecord {
 
     public MealRecord(Paciente paciente, boolean refeicaoSaudavel) { this.paciente = paciente; this.refeicaoSaudavel = refeicaoSaudavel; }
     public boolean isRefeicaoSaudavel() { return refeicaoSaudavel; }
-    public Paciente getPaciente() { return paciente; }
+    public Pessoa getPaciente() { return paciente; }
 }
