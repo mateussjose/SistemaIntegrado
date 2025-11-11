b4bebddb43412ab7ab5295532910f3d6b7f1d145
Mateus Oliveira <76123045+mateussjose@users.noreply.github.com>
2025-11-11 10:59:04 -0300

    Add list for storing 'Pessoa' objects

--- service/DataStore.java
+++ service/DataStore.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DataStore {
+    public List<Pessoa> pessoas = new ArrayList<>();
     public List<Paciente> pacientes = new ArrayList<>();
     public List<Clinica> clinicas = new ArrayList<>();
     public List<Restaurante> restaurantes = new ArrayList<>();
