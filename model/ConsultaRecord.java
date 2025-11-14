b0e9719c587006be5519844402bc6badc7eb84c6
Mateus Oliveira <76123045+mateussjose@users.noreply.github.com>
2025-11-14 10:51:27 -0300

    Update ConsultaRecord.java

--- model/ConsultaRecord.java
+++ model/ConsultaRecord.java
@@ -1,10 +1,22 @@
 package model;
 
+import java.time.LocalDate;
+import java.util.Random;
+
 public class ConsultaRecord {
     private Paciente paciente;
     private String tipoConsulta;
+    private LocalDate data;
 
-    public ConsultaRecord(Paciente paciente, String tipo) { this.paciente = paciente; this.tipoConsulta = tipo; }
+    public ConsultaRecord(Paciente paciente, String tipo) {
+        this.paciente = paciente;
+        this.tipoConsulta = tipo;
+
+        this.data = LocalDate.now().minusDays(new Random().nextInt(7));
+    }
+
     public Paciente getPaciente() { return paciente; }
     public String getTipoConsulta() { return tipoConsulta; }
+    public LocalDate getData() { return data; }
 }
+
